29e537711b45107b39474f4d6965f14ec99d0ae5
Kacper Obara <kacper97pl@tlen.pl>
2019-11-22 10:36:54 +0100

    Initial commit

---
+++ TrackerLibrary/CommentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class CommentModel
+    {
+        public string Content { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+        public string Login  { get; set; }
+        public string Password  { get; set; }
+        public string EmailAdress  { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
